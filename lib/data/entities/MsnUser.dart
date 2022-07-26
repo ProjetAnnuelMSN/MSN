3fe39bc8bf1200751a6c3b5c4dab1a8d9798ace9
Félix <felix@rockett.fr>
2022-07-26 16:07:10 +0200

    Ajout champs manquants User

--- lib/data/entities/MsnUser.dart
+++ lib/data/entities/MsnUser.dart
@@ -5,15 +5,24 @@ class MsnUser {
 
   String password;
 
-  MsnUser(this.email, this.password);
+  String nom;
+
+  String prenom;
+
+  String numeroTel;
+
+  MsnUser(this.email, this.password, this.nom, this.prenom, this.numeroTel);
 
   MsnUser.fromJson(Map<String,Object?> json)
-      : this(json['email']! as String, json['password'] as String);
+      : this(json['email']! as String, json['password'] as String, json['nom'] as String, json['prenom'] as String, json['numeroTel'] as String);
 
   Map<String, dynamic> toJson() {
     return {
       'email': email,
-      'password': password
+      'password': password,
+      'nom': nom,
+      'prenom': prenom,
+      'numeroTel': numeroTel
     };
   }
 
